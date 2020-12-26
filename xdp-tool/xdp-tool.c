5622982393e086eb4ffc7c31e281c45d9121001c
c-sh0 <csh.c0s0h0@gmail.com>
2020-12-26 22:35:50 +0000

    update program description

--- xdp-tool/xdp-tool.c
+++ xdp-tool/xdp-tool.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-static const char *__doc__ = "Simple XDP prog doing XDP_PASS\n";
+static const char *__doc__ = "Simple program to manage XDP programs\n";
 
 #include <stdio.h>
 #include <stdlib.h>
