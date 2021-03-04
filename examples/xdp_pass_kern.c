eee4e2c1c2fb5c57f1a3d055f5bbe3d968e87794
c-sh0 <csh.c0s0h0@gmail.com>
2021-03-04 21:46:07 +0000

    update example kern program

--- examples/xdp_pass_kern.c
+++ examples/xdp_pass_kern.c
@@ -1,16 +1,14 @@
 /*
- *
  * Simple xdp program pass all traffic
  * bpf_printk() messages are written to /sys/kernel/debug/tracing/trace_pipe
- *
  */
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-SEC("simple_pass")
-int xdp_prog(struct xdp_md *ctx) {
+SEC("xdp_pass")
+int xdp_pass_prog(struct xdp_md *ctx) {
 
-	bpf_printk("DEBUG: XDP_SIMPLE PASS ....\n");
+	bpf_printk("DEBUG SIMPLE XDP_PASS");
 	return XDP_PASS;
 }
 
